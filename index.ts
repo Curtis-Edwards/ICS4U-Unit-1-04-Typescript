736e7d3d1af78be21be8c7c04771b2693187c0a4
Curtis Edwards <curtis.edwards@mths.ca>
2024-02-26 20:59:27 +0000

    Fixed formating

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
 /*
- * This is a program that calculates the total cook time based on
- * the lunch item and number of lunch items
+ * This is a program creates a random number 
+ * that the user tries to guess
  *
  * @author  Curtis Edwards
  * @version 1.0
- * @since   2024-02-22
+ * @since   2024-02-26
  */
 
 import { createPrompt } from 'bun-promptx'
 
 // constants & variables
-const randomNumber = Math.floor((Math.random() * 6) + 1);
+const randomNumber = Math.floor((Math.random() * 6) + 1)
 let totalGuesses = 0
 
 while (1) {
   // input
-  const userGuess = createPrompt("Guess a number between 1 - 6:  ")
+  const userGuess = createPrompt("Guess a number between 1 - 6: ")
   const userGuessInt = parseInt(userGuess.value || "-1")
   // error check
   if (isNaN(userGuessInt) == true || userGuessInt < 1 || userGuessInt > 6) {
